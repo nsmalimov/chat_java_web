de5ab7899371e92c48835dadb04175e9376159cf
nsmalimov <norman000@mail.ru>
2016-05-28 16:11:27 +0400

    Update MyServlet.java

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -12,7 +12,6 @@ import java.sql.Statement;
 import javax.sql.DataSource;
 import javax.websocket.server.ServerEndpoint;
 
-///11111
 public class MyServlet extends HttpServlet {
 
     private ServletConfig config;
@@ -20,11 +19,8 @@ public class MyServlet extends HttpServlet {
     String page = "/index.jsp";
 
     public void init(ServletConfig config)
-
             throws ServletException {
-
         this.config = config;
-
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -54,27 +50,17 @@ public class MyServlet extends HttpServlet {
         Connection conn = null;
         try {
             Context ctx = new InitialContext();
-            //Context initContext = new InitialContext();
-
-            //Context envContext  = (Context) initContext.lookup("java:/comp/env");
             DataSource ds = (DataSource) ctx.lookup("java:/comp/env/jdbc/myapp");
 
             conn = ds.getConnection();
             Statement stat = conn.createStatement();
 
-            //stat.executeUpdate("INSERT INTO freeUsers (userKey, isConnected)\n" +
-            //        "VALUES ('123456', 0);");
-            //stat.executeUpdate("INSERT INTO freeUsers (userKey, isConnected)\n" +
-            //        "VALUES ('123456', 0);");
-
             ResultSet rs = stat.executeQuery("select userKey from freeUsers");
             this.name = rs.getString(1);
         } catch (SQLException se) {
             return se.toString();
-            //return "exception 1";
         } catch (NamingException ne) {
             return ne.toString();
-            //return "exception 2";
         } finally {
             try {
                 if (conn != null) conn.close();
@@ -82,9 +68,6 @@ public class MyServlet extends HttpServlet {
                 return e.toString();
             }
         }
-
         return this.name;
     }
-
-
 }
